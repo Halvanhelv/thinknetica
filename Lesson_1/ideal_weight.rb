16cffcb511ffc3dbe580061f4c10118968e0bc75
SynthesisOne <developerfromrussia@gmail.com>
2020-04-14 14:46:50 +0300

    fix

--- Lesson_1/ideal_weight.rb
+++ Lesson_1/ideal_weight.rb
@@ -3,9 +3,9 @@ name = gets.chomp.to_s
 puts 'Введите свой рост в см'
 height = gets.chomp.to_i
 if height <= 0
-
   abort 'Введите нормальный вес'
 end
+
 perfect  =  (height - 110) * 1.15
 if perfect >= 0
 puts "Уважаемый #{name} ваш идеальный вес -- #{perfect} кг"
